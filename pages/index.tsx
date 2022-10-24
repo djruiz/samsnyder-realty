caebcc11b4e97c2bd6827b621e0e513e5eb8bc7b
Daniel Ruiz <djruiz@umail.iu.edu>
2022-10-23 21:07:17 -0400

    About sam section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
-import type { NextPage } from 'next'
-import { Headline } from 'components/headline';
-import { Headshot, headshotColClasses } from 'components/headshot';
-import { Col } from 'react-bootstrap';
-import { Backdrop } from 'components/backdrop';
-import { HomeValueLeadGenForm } from 'components/home-value-lead-gen-form';
+import type { NextPage } from "next";
+import { Headline } from "components/headline";
+import { Headshot, headshotColClasses } from "components/headshot";
+import { Col } from "react-bootstrap";
+import { Backdrop } from "components/backdrop";
+import { HomeValueLeadGenForm } from "components/home-value-lead-gen-form";
+import { About } from "components/about";
 
 const Home: NextPage = () => {
   return (
@@ -18,10 +19,13 @@ const Home: NextPage = () => {
           </Backdrop>
         </Col>
       </Headline>
-      <div className='bg-white position-relative' style={{ minHeight: "100px", zIndex: 1 }}>
-      </div>
+      <div
+        className="bg-white position-relative"
+        style={{ minHeight: "100px", zIndex: 1 }}
+      ></div>
+      <About />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
