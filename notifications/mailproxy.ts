91a82816e3a9c4f026b183fab024f90bd7427732
Daniel Ruiz <djruiz@umail.iu.edu>
2022-11-03 19:31:31 -0400

    Mailer update

--- notifications/mailproxy.ts
+++ notifications/mailproxy.ts
@@ -1,15 +1,17 @@
-import Axios from "axios"
+import Axios from "axios";
 import React from "react";
 import ReactDomServer from "react-dom/server";
 
-export async function sendAdminMail(
-  subject: string,
-  body: React.ReactElement
-) {
-  await Axios.post("https://mailer-o6jtfuuaia-uc.a.run.app/", ReactDomServer.renderToString(body), {
-    headers: {
-      'x-mailer-subject': subject,
-      'x-mailer-to': '<admin>'
+export async function sendAdminMail(subject: string, body: React.ReactElement) {
+  await Axios.post(
+    "https://mailer-o6jtfuuaia-uc.a.run.app/",
+    ReactDomServer.renderToString(body),
+    {
+      headers: {
+        "x-mailer-subject": subject,
+        "x-mailer-to":
+          process.env.NEXT_PUBLIC_ENV == "production" ? "<admin>" : "<dev>",
+      },
     }
-  });
+  );
 }
