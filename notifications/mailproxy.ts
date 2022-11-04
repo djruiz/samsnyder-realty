2eda35043e44fe936d00d29800025f57f8e185ac
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-11-04 19:12:47 +0000

    Don't send emails in development

--- notifications/mailproxy.ts
+++ notifications/mailproxy.ts
@@ -3,18 +3,20 @@ import React from "react";
 import ReactDomServer from "react-dom/server";
 
 export async function sendAdminMail(subject: string, body: React.ReactElement) {
-  const html = ReactDomServer.renderToString(body);
+  if (process.env.NODE_ENV == "production") {
+    const html = ReactDomServer.renderToString(body);
 
-  await Axios.post(
-    "https://mailer-o6jtfuuaia-uc.a.run.app/",
-    { html },
-    {
-      headers: {
-        "X-Mailer-Subject": subject,
-        "X-Mailer-To":
-          process.env.NEXT_PUBLIC_ENV == "production" ? "<admin>" : "<dev>",
-        'Content-Type': 'application/json'
+    await Axios.post(
+      "https://mailer-o6jtfuuaia-uc.a.run.app/",
+      { html },
+      {
+        headers: {
+          "X-Mailer-Subject": subject,
+          "X-Mailer-To":
+            process.env.NEXT_PUBLIC_ENV == "production" ? "<admin>" : "<dev>",
+          'Content-Type': 'application/json'
+        }
       }
-    }
-  );
+    );
+  }
 }
