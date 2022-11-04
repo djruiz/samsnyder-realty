7799eaf9435ec9580fe190bd41b7920efc7aa00b
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-11-04 00:01:15 +0000

    Send mailer body as json object

--- notifications/mailproxy.ts
+++ notifications/mailproxy.ts
@@ -5,13 +5,13 @@ import ReactDomServer from "react-dom/server";
 export async function sendAdminMail(subject: string, body: React.ReactElement) {
   await Axios.post(
     "https://mailer-o6jtfuuaia-uc.a.run.app/",
-    ReactDomServer.renderToString(body),
+    { html: ReactDomServer.renderToString(body) },
     {
       headers: {
         "x-mailer-subject": subject,
         "x-mailer-to":
           process.env.NEXT_PUBLIC_ENV == "production" ? "<admin>" : "<dev>",
-        'content-type': 'text/plain'
+        'content-type': 'application/json'
       }
     }
   );
