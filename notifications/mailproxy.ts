e0ed4be34a63ad09e9036bb5e16b2da8af2b4b1d
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-11-04 01:18:04 +0000

    Update axios headers for mailer

--- notifications/mailproxy.ts
+++ notifications/mailproxy.ts
@@ -3,15 +3,19 @@ import React from "react";
 import ReactDomServer from "react-dom/server";
 
 export async function sendAdminMail(subject: string, body: React.ReactElement) {
+  const html = ReactDomServer.renderToString(body);
+
+  console.log(html);
+
   await Axios.post(
     "https://mailer-o6jtfuuaia-uc.a.run.app/",
-    { html: ReactDomServer.renderToString(body) },
+    { html },
     {
       headers: {
-        "x-mailer-subject": subject,
-        "x-mailer-to":
+        "X-Mailer-Subject": subject,
+        "X-Mailer-To":
           process.env.NEXT_PUBLIC_ENV == "production" ? "<admin>" : "<dev>",
-        'content-type': 'application/json'
+        'Content-Type': 'application/json'
       }
     }
   );
