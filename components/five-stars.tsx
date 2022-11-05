5ed417b63c391e0b8ff571b0b9863dbbcd401a8c
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-11-05 06:13:44 +0000

    Adjust star color

--- components/five-stars.tsx
+++ components/five-stars.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export const FiveStars = () => {
   return (
     <div className="d-flex drop-shadow-sm">
-      {Array.from({ length: 5 }).map((_, i) => <FontAwesomeIcon key={i} icon={faStar} style={{ color: "gold" }} />)}
+      {Array.from({ length: 5 }).map((_, i) => <FontAwesomeIcon key={i} icon={faStar} style={{ color: "#fcb203" }} />)}
     </div>
   )
 }
