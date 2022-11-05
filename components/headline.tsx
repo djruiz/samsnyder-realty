32940819f03ab5524a36ab028dd215979d849bd2
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-11-05 02:15:39 +0000

    Fix headline double Row component

--- components/headline.tsx
+++ components/headline.tsx
@@ -5,9 +5,7 @@ export const Headline: Component = ({ children }) => {
   return (
     <div className='w-100' style={{ background: `linear-gradient(90deg, #0024A150, #00000000, #FFA48750), url("${FortWayneImage.src}")`, backgroundSize: "cover", boxShadow: "inset 0 0 15px 5px #00000050" }}>
       <div className='container mw-1100'>
-        <div className='row'>
-          {children}
-        </div>
+        {children}
       </div>
     </div>
   )
