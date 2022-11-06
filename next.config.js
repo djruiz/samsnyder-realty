9868f897c8587b3c79ae12b00f36e4ecd63cecd4
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-11-06 15:55:28 +0000

    Add base path env var

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   swcMinify: true,
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
