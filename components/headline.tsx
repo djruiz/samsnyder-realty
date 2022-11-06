53621f3b4a22d13caa891d47ea9d928b01a3b251
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-11-06 23:12:25 +0000

    Make healine image next gen

--- components/headline.tsx
+++ components/headline.tsx
@@ -1,9 +1,11 @@
 import { Component } from "types/component";
 import FortWayneImage from "assets/fort-wayne.webp";
+import { NextGenImage } from "./next-gen-image";
 
 export const Headline: Component = ({ children }) => {
   return (
-    <div className='w-100' style={{ background: `linear-gradient(90deg, #0024A150, #00000000, #FFA48750), url("${FortWayneImage.src}"), url(${FortWayneImage.src.replace(/\.(webp)($|\?)/, '.png$2')})`, backgroundSize: "cover", boxShadow: "inset 0 0 15px 5px #00000050" }}>
+    <div className='w-100' style={{ background: 'linear-gradient(90deg, #0024A150, #00000000, #FFA48750)', backgroundSize: "cover", boxShadow: "inset 0 0 15px 5px #00000050" }}>
+      <NextGenImage width="100vw" style={{ position: "relative", zIndex: -1 }} layout="fill" objectPosition="cover" src={FortWayneImage} />
       <div className='container mw-1100'>
         {children}
       </div>
