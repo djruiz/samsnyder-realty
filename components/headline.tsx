82489c8ee2f28198cf9475df4de07985fcf5fd16
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-11-07 14:57:20 +0000

    Fix contact field bug

--- components/headline.tsx
+++ components/headline.tsx
@@ -6,7 +6,7 @@ export const Headline: Component = ({ children }) => {
   return (
     <div className='w-100 position-relative' style={{ background: 'linear-gradient(90deg, #0024A150, #00000000, #FFA48750)', backgroundSize: "cover", boxShadow: "inset 0 0 15px 5px #00000050" }}>
       <NextGenImage alt="Fort Wayne Skyline" style={{ position: "absolute", zIndex: -1 }} layout="fill" objectFit="cover" objectPosition={"top"} src={FortWayneImage} />
-      <div className='container mw-1100'>
+      <div style={{ position: "relative", zIndex: 0 }} className='container mw-1100'>
         {children}
       </div>
     </div>
